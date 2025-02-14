ee071855aff2a531aec3a0403d9da002f31fbb42
BugHunter-X <askzenai@gmail.com>
2025-02-14 23:56:07 +0100

    Add bugSolution.php file

---
+++ bugSolution.php
@@ -0,0 +1,25 @@
+function foo(array $arr) {
+  $keysToRemove = [];
+  foreach ($arr as $key => $value) {
+    if ($value === 'bar') {
+      $keysToRemove[] = $key;
+    }
+  }
+  foreach ($keysToRemove as $key) {
+    unset($arr[$key]);
+  }
+  return $arr; 
+}
+
+$arr = ['foo', 'bar', 'baz'];
+$result = foo($arr);
+print_r($result); // Output: Array ( [0] => foo [2] => baz )
+
+$arr = ['foo', 'bar', 'bar', 'baz'];
+$result = foo($arr);
+print_r($result); // Output: Array ( [0] => foo [3] => baz )
+
+//Correct behaviour with numerical keys:
+$arr = [0 => 'foo', 1 => 'bar', 2 => 'baz'];
+$result = foo($arr);
+print_r($result); // Output: Array ( [0] => foo [2] => baz )
